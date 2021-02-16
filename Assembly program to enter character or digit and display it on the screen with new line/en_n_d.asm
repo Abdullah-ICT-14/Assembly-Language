3ed4702c7695a616e5eaf01a086bc3ffacab2850
Abdullah-ICT-14 <mdabdullah01728@gmail.com>
2021-02-16 21:21:14 +0600

    Enter digits or Character

---
+++ Assembly program to enter character or digit and display it on the screen with new line/en_n_d.asm
@@ -0,0 +1,22 @@
+.model small
+.stack 100h
+.data
+.code
+main proc
+    mov ah,1
+    int 21h
+
+    mov bl,al
+    int 21h
+
+    mov ah,2
+    mov dl,0ah
+    int 21h
+
+    mov dl,bl
+    int 21h
+
+    mov ah,4ch
+    int 21h
+    main endp
+end main
