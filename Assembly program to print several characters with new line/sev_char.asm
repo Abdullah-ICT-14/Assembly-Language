b8699ddf31db417b8c5a4da3cec3116a8f05dc5a
Abdullah-ICT-14 <mdabdullah01728@gmail.com>
2021-02-16 20:45:41 +0600

    Print several characters with new lines

---
+++ Assembly program to print several characters with new line/sev_char.asm
@@ -0,0 +1,25 @@
+.model small
+.stack 100h
+.data
+.code
+main proc
+mov ah,2
+mov dl,'P'
+int 21h
+
+mov ah,2
+mov dl,0ah
+int 21h
+
+mov ah,2
+mov dl,0ah
+int 21h
+
+mov ah,2
+mov dl,'A'
+int 21h
+
+mov ah,4ch
+int 21h
+main endp
+end main
